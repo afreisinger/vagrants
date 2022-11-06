f57d8be08ed9986e09155dfbf83ce2f04acc9daf
Adrian Freisinger <afreisinger@gmail.com>
2022-11-06 13:50:10 -0300

    change local.com.conf

--- openemr/provision/components/apache.sh
+++ openemr/provision/components/apache.sh
@@ -4,13 +4,13 @@ apt-get update
 apt-get install -y apache2
 
 # Copy the vhost config file
-cp /var/www/provision/config/apache/vhosts/mytestproject.local.com.conf /etc/apache2/sites-available/mytestproject.local.com.conf
+cp /var/www/provision/config/apache/vhosts/openemr.local.com.conf /etc/apache2/sites-available/openemr.local.com.conf
 
 # Disable the default vhost file
 a2dissite 000-default
 
 # Enable our custom vhost file
-a2ensite mytestproject.local.com.conf
+a2ensite openemr.local.com.conf
 
 # Restart for the changes to take effect
 service apache2 restart
