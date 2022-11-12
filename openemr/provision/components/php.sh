586bd3adb194a8f2ddc25bb9ebaf19cdf254eabd
Adrian Freisinger <afreisinger@gmail.com>
2022-11-12 12:37:22 -0300

    add php7.4-gd library

--- openemr/provision/components/php.sh
+++ openemr/provision/components/php.sh
@@ -3,7 +3,7 @@
 sudo apt-get install software-properties-common
 sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 sudo apt-get update
-sudo apt-get install -y php7.4 php7.4-bcmath php7.4-bz2 php7.4-cli php7.4-curl php7.4-intl php7.4-json php7.4-mbstring php7.4-opcache php7.4-soap php7.4-xml php7.4-xsl php7.4-zip libapache2-mod-php7.4 php7.4-mysql composer
+sudo apt-get install -y php7.4 php7.4-bcmath php7.4-bz2 php7.4-cli php7.4-curl php7.4-intl php7.4-json php7.4-mbstring php7.4-opcache php7.4-soap php7.4-xml php7.4-xsl php7.4-zip libapache2-mod-php7.4 php7.4-mysql php7.4-gd composer
 
 #https://www.open-emr.org/wiki/index.php/FAQ#What_are_the_correct_PHP_settings_.28can_be_found_in_the_php.ini_file.29.3F
 
